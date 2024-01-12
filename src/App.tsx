fde0bb77dcd80d2523d6e66b298a8889917ea945
intes-it <secur365.suite@gmail.com>
2024-01-12 16:06:58 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Router from "./Router";
+import { MantineProvider } from "@mantine/core";
+function App() {
+  return (
+    <MantineProvider>
+      <Router />
+    </MantineProvider>
+  );
+}
+
+export default App;
